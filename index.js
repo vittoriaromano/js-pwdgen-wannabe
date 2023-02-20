5bc00346284a7fbcaea6b0c9fadc0542e1fdbd6d
vittoriaromano <vittoria5005@gmail.com>
2023-02-20 17:24:16 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// resetto la pagina
+
+
+
+// aggiungo alert
+
+alert("benvenuto");
+
+// aggiungo i prompt e li visualizzo su console
+        // nome
+const nome = prompt("come ti chiami?");
+console.log(nome);
+       // cognome
+const cognome = prompt("e di cognome?");
+console.log(cognome);
+       // colore preferito
+const colorePreferito = prompt("il tuo colore preferito?");
+console.log(colorePreferito);
